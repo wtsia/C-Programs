1847cbe092556191a3fd770e88c32162ce8e1eb9
Winston <winstontsia@gmail.com>
2024-05-17 03:06:27 -0700

    updating

--- finProgramSol.c
+++ finProgramSol.c
@@ -11,7 +11,11 @@ void PrintArray(int* array, int size) {
 // fibonacci(0) = 0, fibonacci(1) = 1, fibonacci(2) = 1
 // Ex: n = 5, seq = 0 1 1 2 3
 int* Fibonacci(int n) {
-    int* seq = malloc(n * sizeof(int)); // Allocate memory for n integers
+    int* seq;
+    int j;
+
+   /* Type your code here. */
+    seq = malloc(n * sizeof(int)); // Allocate memory for n integers
     if (n > 0) seq[0] = 0; // First Fibonacci number
     if (n > 1) seq[1] = 1; // Second Fibonacci number
 
@@ -25,8 +29,10 @@ int* Fibonacci(int n) {
 // Return sequence of squares for 1..n (inclusive)
 // Ex: sqrn = 3, seq = 1 4 9
 int* Squares(int n) {
-    int* seq = malloc(n * sizeof(int)); // Allocate memory for n integers
+    int* seq; // Allocate memory for n integers
 
+   /* Type your code here. */
+    seq = malloc(n * sizeof(int));
     for (int j = 0; j < n; ++j) {
         seq[j] = (j + 1) * (j + 1); // Square of (j + 1)
     }
@@ -35,7 +41,11 @@ int* Squares(int n) {
 
 // Return an array that is a copy of array1 followed by the elements of array2
 int* Concatenate(int* array1, int size1, int* array2, int size2) {
-    int* seq = malloc((size1 + size2) * sizeof(int)); // Allocate memory for size1 + size2 integers
+    int j;
+    int* seq;
+
+   /* Type your code here. */
+    seq = malloc((size1 + size2) * sizeof(int)); // Allocate memory for size1 + size2 integers
 
     for (int j = 0; j < size1; ++j) {
         seq[j] = array1[j]; // Copy elements of array1
