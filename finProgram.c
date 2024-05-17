355e5716f5c7a902c31c8527e7d9ed834278d12a
wtsia <winstontsia@gmail.com>
2024-05-17 13:20:18 -0700

    updating template

--- finProgram.c
+++ finProgram.c
@@ -8,44 +8,47 @@ void PrintArray(int* array, int size) {
 }
 
 // Return the first n Fibonacci numbers
-// fibonacci(0) = 0, fibonacci(1) = 1, fibonacci(2) = 1
-// Ex: n = 5, seq = 0 1 1 2 3
 int* Fibonacci(int n) {
-   int* seq;
-   int j;
+   int* seq = malloc(n * sizeof(int)); // Allocate memory for n integers
+   if (n > 0) seq[0] = 0; // First Fibonacci number
+   if (n > 1) seq[1] = 1; // Second Fibonacci number
 
-   /* Type your code here. */
+   for (int j = 2; j < n; ++j) {
+      seq[j] = seq[j - 1] + seq[j - 2];
+   }
 
    return seq;
 }
 
 // Return sequence of squares for 1..n (inclusive)
-// Ex: sqrn = 3, seq = 1 4 9
 int* Squares(int n) {
-   int* seq;
+   int* seq = malloc(n * sizeof(int)); // Allocate memory for n integers
 
-   /* Type your code here. */
+   for (int j = 0; j < n; ++j) {
+      seq[j] = (j + 1) * (j + 1); // Square of (j + 1)
+   }
 
    return seq;
 }
 
-// Return an array that is a copy of array1 followed by
-// the elements of array2
+// Return an array that is a copy of array1 followed by the elements of array2
 int* Concatenate(int* array1, int size1, int* array2, int size2) {
-   int j;
-   int* seq;
+   int* seq = malloc((size1 + size2) * sizeof(int)); // Allocate memory for size1 + size2 integers
 
-   /* Type your code here. */
+   for (int j = 0; j < size1; ++j) {
+      seq[j] = array1[j]; // Copy elements of array1
+   }
+   for (int j = 0; j < size2; ++j) {
+      seq[size1 + j] = array2[j]; // Copy elements of array2
+   }
 
    return seq;
 }
 
 int main(void) {
 
-   int fibn;                                 // seq of first fibn Fibonacci numbers
-                                             // Ex: fibn = 5, seq = 0 1 1 2 3
-   int sqrn;                                 // number of squares starting with 1
-                                             // Ex: sqrn = 3, seq = 1 4 9
+   int fibn; // Number of Fibonacci numbers
+   int sqrn; // Number of squares
    scanf("%d %d", &fibn, &sqrn);
 
    int* fibs;
@@ -63,6 +66,11 @@ int main(void) {
    conc = Concatenate(fibs, fibn, sqrs, sqrn);
    PrintArray(conc, fibn + sqrn);
    printf("\n");
+
+   // Free the dynamically allocated memory
+   free(fibs);
+   free(sqrs);
+   free(conc);
 
    return 0;
 }
